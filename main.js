65c5c4c7184f370d14194c065b7bbdaed4f78f39
Martin <martin@brunninger.net>
2020-08-26 16:26:21 +0200

    PauseIndicator Code aus Hexxen1733 übernommen und um Position Top erweitert

---
+++ main.js
@@ -0,0 +1,14 @@
+/**
+ * Author: Martin Brunninger
+ * Software License: GNU GPLv3
+ */
+
+/* -------------------------------------------- */
+/*  Foundry VTT Initialization                  */
+/* -------------------------------------------- */
+
+Hooks.once("init", async function() {
+
+  TweakPauseIndicator.registerSettings();
+
+});
